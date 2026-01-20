bd16c0d8cb9e1a70f20efccf63f345026fc3483c
Bayo12JaMorant <bayoibironke1@outlook.com>
2026-01-20 14:53:56 -0600

    Starting Menu with case esac, Created empty functions

--- 5thGrader.sh
+++ 5thGrader.sh
@@ -1,9 +1,37 @@
 #!/bin/bash
+clear
 
 
 
+for_Math(){
+
+}
+
+for_Science(){
+
+}
+
+for_Geography(){
+
+}
+
+for_History(){
+
+}
+
+for_Trivia(){
+
+}
 
 
+for_Menu(){
+   
+	echo -e "
+	 Welcome to: Are You Smarter Than a 5th Grader?
+     - - - - - - - - - -- - -- - -- - -- - - -- -        
+	 Want to know how smart you really are? Lets find out! 
+	"
 
+	read -p "What Subject would you like to be tested on? (1-5) " answer
 
-echo -e "Are You Smarter than 5th Grader? time to find out! "
+}
